0f7121cc8941867a3bea1a9a981f0594d21c8a93
Risusama <12206978+Risusama@users.noreply.github.com>
2022-11-16 01:19:26 +0200

    Fine tuning, pt.1
    Adjusted many brush sizes to reduce file size and compile time.
    
    Also added Batch compile script for Windows with semi-generalized paths.

---
+++ maps/compile_map.bat
@@ -0,0 +1,3 @@
+%userprofile%/pelit/netradiant_1.5.0/q3map2.exe -fs_game truecombat -fs_basepath "%userprofile%/pelit/openarena-0.8.8" -meta -v "cetape.map"
+%userprofile%/pelit/netradiant_1.5.0/q3map2.exe -fs_game truecombat -fs_basepath "%userprofile%/pelit/openarena-0.8.8" -vis -v "cetape.map"
+%userprofile%/pelit/netradiant_1.5.0/q3map2.exe -fs_game truecombat -fs_basepath "%userprofile%/pelit/openarena-0.8.8" -light -fast -patchshadows -samples 3 -bounce 8 -gamma 2 -compensate 4 -dirty -v "cetape.map"
