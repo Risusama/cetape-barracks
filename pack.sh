a336d33801fbba1cebc883ad107dd5c42b21b789
Risusama <12206978+Risusama@users.noreply.github.com>
2025-12-02 21:34:00 +0200

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,2 +1,20 @@
-zip -r "cetape-barracks.zip" . -x '*.git*'
-mv "cetape-barracks.zip" "cetape-barracks.pk3"
+#!/bin/sh
+
+set -eu
+
+RUNARGS=""
+if [ "$#" -gt 0 ]; then
+  RUNARGS="$1"
+else 
+  echo "No argumets given, please run again with arguments 'truecombat' or 'wolf' to pack."
+  exit
+fi
+
+if [ "$RUNARGS" = "truecombat" ]; then
+  zip -r "cetape-barracks.pk3" . -x '.git*' 'netradiant/*/*' 'pack.sh' 'maps/*.autosave.map' 'maps/*.bak' 'maps/cetape_et*'
+elif [ "$RUNARGS" = "wolf" ]; then
+  zip -r "cetape-barracks.pk3" . -x '.git*' 'netradiant/*/*' 'pack.sh' 'maps/*.autosave.map' 'maps/*.bak' 'maps/cetape.*'
+else
+  echo "Invalid argument. Exiting."
+  exit
+fi
